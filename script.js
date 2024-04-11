c4bddb0e091e0bc370efa2930f1c4486926280c5
556887FF <148990184+556887FF@users.noreply.github.com>
2024-04-11 21:22:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // Define an array of image URLs
 const images = [
-  'https://yt3.ggpht.com/ytc/AIdro_kRGBwx4M004I3nnpcYtfKBIjVqXnAuQYGL1-9DQj7iVuIZ=s88-c-k-c0x00ffffff-no-rj',
-  'https://yt3.ggpht.com/ytc/AIdro_mF8m285i9VNEL4LDoongeaxJHCdQVpb2RRhxVZAgGSrcxc=s88-c-k-c0x00ffffff-no-rj',
-  'https://yt3.ggpht.com/KB7kfb9MMMOC4U9svp4dfYb4aziWKh1wxWyzWBoAuhNjpz8DcFKMdeumtiaprUHpajUxuEmtSg=s88-c-k-c0x00ffffff-no-rj'
+  'https://www.example.com/image1.jpg',
+  'https://www.example.com/image2.jpg',
+  'https://www.example.com/image3.jpg'
 ];
 
 let currentIndex = 0;
 const imageElement = document.getElementById('image');
+const imageContainer = document.getElementById('imageContainer');
+const passwordForm = document.getElementById('passwordForm');
 
 function changeImage() {
   imageElement.src = images[currentIndex];
@@ -18,3 +20,22 @@ changeImage();
 
 // Set interval to change image every 3 seconds (3000 milliseconds)
 setInterval(changeImage, 3000);
+
+// Function to check password
+function checkPassword() {
+  const passwordInput = document.getElementById('password');
+  const password = passwordInput.value;
+
+  if (password === '580777') {
+    // Show image container if password is correct
+    imageContainer.style.display = 'block';
+    // Clear password input
+    passwordInput.value = '';
+    // Hide password form
+    passwordForm.style.display = 'none';
+  } else {
+    alert('Incorrect password. Please try again.');
+    // Clear password input
+    passwordInput.value = '';
+  }
+}
